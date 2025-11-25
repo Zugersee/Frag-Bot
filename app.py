0dc251d294beb708f69dbe06a2e22be190ba5c95
Zugersee <149376091+Zugersee@users.noreply.github.com>
2025-11-25 08:06:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st 
 import google.generativeai as genai 
 st.title("Reparatur")
-enai.configure(api_key=st.secrets["GOOGLE_API_KEY"]) 
+genai.configure(api_key=st.secrets["GOOGLE_API_KEY"]) 
 t.write("Ich suche Modelle...") 
 for m in genai.list_models():    
     st.write(m.name)
